465cc396c49a0bbced234fc2d0f2b99bd4d52867
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-16 07:18:57 +0530

    implement dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
+import { ModeToggle } from "./components/mode-toggle";
 import { Button } from "./components/ui/button";
 
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
       <Button>
         Click Me
       </Button>
+      <ModeToggle />
     </div>
   )
 }
