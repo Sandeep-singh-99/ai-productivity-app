5905fdcd075b6f26e3024a697c3aaa3e3ec84815
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-17 16:10:33 +0530

    refactor: destructure req.body for improved readability in clerkWebhooks

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -10,7 +10,7 @@ export const clerkWebhooks = async (req, res) => {
             "svix-signature": req.headers["svix-signature"]
         });
 
-        const [data, type] = req.body;
+        const { data, type } = req.body;
 
         switch(type) {
             case "user.created": {
