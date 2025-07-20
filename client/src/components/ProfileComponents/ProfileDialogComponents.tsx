40ecd8565c7d92aeb22080b243cf03eec36b34b3
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-20 11:26:29 +0530

    feat: Refactor ProfileDialogComponents to display user profile details and remove unnecessary form elements

--- client/src/components/ProfileComponents/ProfileDialogComponents.tsx
+++ client/src/components/ProfileComponents/ProfileDialogComponents.tsx
@@ -1,55 +1,51 @@
-import { Button } from "@/components/ui/button"
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { useAppSelector } from "@/hooks/hooks";
 import type { ReactNode } from "react";
+import { Button } from "../ui/button";
+
 
 interface ProfileDialogComponentsProps {
   triggerButton: ReactNode;
 }
 
 export default function ProfileDialogComponents({ triggerButton }: ProfileDialogComponentsProps) {
+  const { user } = useAppSelector((state) => state.auth);
   return (
      <Dialog>
-      <form>
-        <DialogTrigger asChild>
-            {triggerButton}
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+      <DialogTrigger asChild>
+        {triggerButton}
+      </DialogTrigger>
+      <DialogContent className="bg-[#212126] md:min-w-3xl sm:min-w-xl">
+        <DialogHeader>
+          <DialogTitle>
+            Profile Details
+          </DialogTitle>
+          <DialogDescription>
+            User profile information and update options.
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center space-x-4">
+              <img className="w-16 h-16 rounded-full border-2 shadow-2xl" src={user?.imageUrl || ""} alt="User Profile Picture"/>
+              <div>
+                <h3 className="text-xl font-semibold">{user?.firstName} {user?.lastName}</h3>
+                <p className="text-sm text-muted-foreground">{user?.email}</p>
+              </div>
             </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
-            </div>
+            <Button variant={"outline"}>
+              Edit Profile
+            </Button>
           </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
+        </div>
+      </DialogContent>
     </Dialog>
   )
 }
