88a8f5e7f316f6dad84031d16557d0119a1ef7c6
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-24 22:31:23 +0530

    feat: enhance ArticleForm and ArticleView with loading state and error handling; update article slice for improved state management

--- client/src/components/ArticleComponents/ArticleForm.tsx
+++ client/src/components/ArticleComponents/ArticleForm.tsx
@@ -3,20 +3,53 @@ import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
+import React, { useState } from "react";
+import { useGenerateArticle } from "@/api/aiApi";
+import { useAppDispatch } from "@/hooks/hooks";
+import { setArticle, setLoading } from "@/redux/slice/articleSlice";
+import { toast } from "react-toastify";
+import { AxiosError } from "axios";
 
 
 export default function ArticleForm() {
+  const [question, setQuestion] = useState<string>("");
+
+  const articleMutation = useGenerateArticle();
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(setLoading(true));
+
+    if (!question) {
+      toast.error("Please enter a question.");
+      dispatch(setLoading(false));
+      return;
+    }
+
+    try {
+      const result = await articleMutation.mutateAsync({ question });
+      dispatch(setArticle(result));
+      dispatch(setLoading(false));
+      toast.success(result.message || "Article generated successfully!");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message || "An error occurred while generating the article.");
+      }
+    }
+  }
+
   return (
     <Card className="w-full h-full">
         <CardHeader className="flex items-center space-x-2">
             <Sparkles className="h-4 w-4 mr-2" size={52} />
             <h2 className="text-lg font-semibold">Article Configuration</h2>
         </CardHeader>
-        <form>
+        <form onSubmit={handleSubmit}>
         <CardContent className="mb-5">
            <div className="space-y-4">
              <Label className="font-semibold">Article Topic</Label>
-            <Textarea rows={5} placeholder="The future of artificial intelligence is..."/>
+            <Textarea value={question} onChange={(e) => setQuestion(e.target.value)} rows={5} placeholder="The future of artificial intelligence is..."/>
            </div>
         </CardContent>
 
--- client/src/redux/slice/articleSlice.ts
+++ client/src/redux/slice/articleSlice.ts
@@ -23,6 +23,7 @@ const initialState: IArticleResponse = {
     article: null
 }
 
+
 const articleSlice = createSlice({
     name: "article",
     initialState,
